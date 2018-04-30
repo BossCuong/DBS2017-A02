affae8c31af35066d9345ed69413d2e370631906
Cuong Bui <1610342@hcmut.edu.vn>
2018-04-30 17:24:17 +0700

    company route done

--- routes/company.js
+++ routes/company.js
@@ -1,12 +1,4 @@
-//--------------------------------render company profile--------------------------------
-exports.profile = function (req, res) {
-    var message = '';
-    console.log(req.params.companyName);
-    res.render('company_profile.ejs', {
-        message: message
-    });
-
-};
+//---------------------------------company page call-------------------------------
 exports.company = function (req, res) {
     var message = '';
     var sql = "select * from company";
@@ -19,6 +11,26 @@ exports.company = function (req, res) {
         }
     });
 
+};
+
+//--------------------------------render company profile--------------------------------
+exports.profile = function (req, res) {
+
+    //Get request parameter
+    var companyName = req.params.companyName;
+
+    //SQL query
+    var sql = SqlString.format("SELECT * FROM company WHERE name = ?", [companyName]);
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+
+        if (result.length > 0) {
+            res.render('company_profile.ejs', {
+                data: result
+            });
+        } else res.send('Page not found');
+    });
 };
 
 exports.db = function (req, res) {
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express = require('express'),
     job = require('./routes/job'),
     search = require('./routes/search'),
     http = require('http'),
+    SqlString = require('sqlstring'),
     path = require('path');
 //var methodOverride = require('method-override');
 var session = require('express-session');
@@ -24,7 +25,7 @@ var connection = mysql.createConnection({
 connection.connect();
 
 global.db = connection;
-
+global.SqlString = SqlString;
 // all environments
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
@@ -43,6 +44,7 @@ app.use(session({
     }
 }))
 
+//Simple log
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
@@ -55,19 +57,28 @@ app.get('/', company.db); //call for da post
 
 // development only
 
-
+//-------------------------------------------------Signup page------------------------------------------------------------
 app.get('/signup', user.signup); //call for signup page
 app.post('/signup', user.signup); //call for signup post 
-app.post('/login', user.login); //call for login post
+
+//-------------------------------------------------Login page------------------------------------------------------------
 app.get('/login', user.login); //call for login page
+app.post('/login', user.login); //call for login post
+
+//-------------------------------------------------After Login page------------------------------------------------------------
 app.get('/dashboard', user.dashboard); //call for dashboard page after login
 app.get('/logout', user.logout); //call for logout
 app.get('/profile', user.profile); //to render users profile
 
-app.get('/company/:companyName', company.profile); //to render company
-app.get('/company', company.company);
+//-------------------------------------------------Company page------------------------------------------------------------
+app.get('/company/:companyName', company.profile); //call for company profile
+app.get('/company', company.company); //call for company list page
+
+//-------------------------------------------------Search page------------------------------------------------------------
 app.post('/search', search.search);
 app.get('/search', search.search);
+
+
 app.get('/job', job.job); //call for job page
 //Middleware
-app.listen(8080)
+app.listen(8080);
